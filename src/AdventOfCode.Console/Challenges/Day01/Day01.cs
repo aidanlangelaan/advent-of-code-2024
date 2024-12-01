63c597f31e07a02fcda3c5477e4301ae2d51b8d8
Aidan Langelaan <aidanlangelaan@gmail.com>
2024-12-01 06:47:46 +0100

    Day 1 part 1 and 2

---
+++ src/AdventOfCode.Console/Challenges/Day01/Day01.cs
@@ -0,0 +1,47 @@
+﻿using AdventOfCode.Core.Classes;
+
+namespace AdventOfCode.Challenges;
+
+public class Day01 : SolutionBase
+{
+    public override int Day => 01;
+
+    public override object PartOne(string[] input)
+    {
+        var (listA, listB) = ParseLists(input);
+
+        var diffs = new List<int>();
+        while (listA.Count > 0)
+        {
+            var a = listA.Min();
+            var b = listB.Min();
+
+            diffs.Add(Math.Abs(a - b));
+
+            listA.Remove(a);
+            listB.Remove(b);
+        }
+
+        return diffs.Sum();
+    }
+
+    public override object PartTwo(string[] input)
+    {
+        var (listA, listB) = ParseLists(input);
+        return listA.Sum(num => num * listB.Count(x => x == num));
+    }
+
+    private static (List<int> listA, List<int> listB) ParseLists(string[] input)
+    {
+        var listA = new List<int>();
+        var listB = new List<int>();
+        foreach (var line in input)
+        {
+            var nums = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            listA.Add(nums[0]);
+            listB.Add(nums[1]);
+        }
+
+        return (listA, listB);
+    }
+}
